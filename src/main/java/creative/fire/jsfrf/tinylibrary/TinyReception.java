3b98ac0ef4cdcb41059c35d5003b58d961f7bddf
feuyeux <feuyeux@gmail.com>
2012-04-09 01:18:59 +0800

    step3

---
+++ src/main/java/creative/fire/jsfrf/tinylibrary/TinyReception.java
@@ -0,0 +1,28 @@
+package creative.fire.jsfrf.tinylibrary;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+
+/**
+ * @author feuyeux@gmail.com
+ * @version 1.0
+ */
+@ManagedBean(name = "reception")
+@RequestScoped
+public class TinyReception {
+	@ManagedProperty(value="尹天仇")
+	private String name;
+
+	public TinyReception() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
